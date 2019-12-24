15a396b8ad99dd949d83b7b314911af900e37b0e
panoued <panoued@163.com>
2019-12-24 17:21:36 +0800

    create files

---
+++ src/entry-server.js
@@ -0,0 +1,12 @@
+// 渲染首屏
+import createApp from "./app";
+
+export default context => {
+  return new Promise((resolve, reject) => {
+    const { app, router } = createApp();
+    router.push(context.url)
+    router.onReady(() => {
+        resolve(app);
+    }, reject)
+  });
+};
